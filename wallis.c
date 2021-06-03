f29599988a95a7d83ee545bee0a77e9480ee7011
gopal265 <84660174+gopal265@users.noreply.github.com>
2021-06-03 13:27:03 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -24,15 +24,15 @@ int main(void) {
   }
 }
 
-float wallis_pi(int n) 
-{ 
- float k=1.0;
-    int i;
-      for(i=1;i<=n;i++)
-        { 
-            k*=(float)(4.*i*i)/(4.*i*i-1);
-         }
-  return (k*2); 
-} 
+float wallis_pi(int n)
+{
+float k=1.0;
+   int i;
+    for(i=1;i<=n;i++) 
+     {
+        k *=(float)(4.*i*i)/(4.*i*i-1);
+       }
 
+  return (k*2);
+}
 
